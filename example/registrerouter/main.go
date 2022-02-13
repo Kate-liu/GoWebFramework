5455cb68555bbdd5de8ed0ed9d693b7ecb5764e8
rmliu <2218207429@qq.com>
2022-02-13 14:51:11 +0800

    feat(router): add go router regulation code content

--- example/registrerouter/main.go
+++ example/registrerouter/main.go
@@ -5,22 +5,17 @@ import "github.com/Kate-liu/GoWebFramework/framework"
 // 注册路由规则
 func registerRouter(core *framework.Core) {
 	// 需求1+2:HTTP方法+静态路由匹配
-	core.Get("/user/login", UserLoginController)
+	core.Post("/user/login", UserLoginController)
 
 	// 需求3:批量通用前缀
 	subjectApi := core.Group("/subject")
 	{
+		subjectApi.Post("/add", SubjectAddController)
+
 		// 需求4:动态路由
 		subjectApi.Delete("/:id", SubjectDelController)
 		subjectApi.Put("/:id", SubjectUpdateController)
 		subjectApi.Get("/:id", SubjectGetController)
 		subjectApi.Get("/list/all", SubjectListController)
-
-		// 思考题：批量通用前缀
-		subjectInnerApi := subjectApi.Group("/info")
-		{
-			subjectInnerApi.Get("/name", SubjectNameController)
-		}
 	}
-
 }
